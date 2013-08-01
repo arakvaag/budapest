a14b00f6de4305f5fe9bf7c756f28c995c511ce9
anslaae <anslaae@hotmail.com>
2013-08-01 13:05:32 +0200

    Legger til javakode og dependency

--- budapest/grails-app/conf/spring/resources.groovy
+++ budapest/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,8 @@
-// Place your Spring DSL code here
+import org.rakvag.spotifyapi.HTTPBufferedReader
+import org.rakvag.spotifyapi.SpotifyAPIImpl;
+
 beans = {
+	spotifyAPI(SpotifyAPIImpl){
+		httpReader = new HTTPBufferedReader()
+	}
 }
